05e691a975fad3e2e9589658eb3eb168be8c167d
祁嘉俊 <1481700559@qq.com>
2025-05-19 17:47:37 +0800

    feat: 添加sharp生成缩略图

--- app/controller/utils.ts
+++ app/controller/utils.ts
@@ -1,5 +1,7 @@
 import { Controller } from "egg"
 import path from "path"
+import sharp from "sharp"
+import { parse } from "path"
 
 function formatPath(p: string) {
   if (!p) {
@@ -17,9 +19,22 @@ export default class UtilsController extends Controller {
   async fileLocalUpload() {
     const { ctx } = this
     const file = ctx.request.files[0]
-    const url = formatPath(
-      file.filepath.replace(ctx.app.config.baseDir, ctx.app.config.baseUrl)
+    const { filepath } = file
+    const imageSource = sharp(filepath)
+    const metaData = await imageSource.metadata()
+    const { width } = metaData
+    let url = formatPath(
+      filepath.replace(ctx.app.config.baseDir, ctx.app.config.baseUrl)
     )
+    if (width && width > 300) {
+      // 生成一个新的路径
+      const { name, ext, dir } = parse(filepath)
+      const newPath = path.join(dir, `${name}-thumb${ext}`)
+      await imageSource.resize({ width: 300 }).toFile(newPath)
+      url = formatPath(
+        newPath.replace(ctx.app.config.baseDir, ctx.app.config.baseUrl)
+      )
+    }
     ctx.helper.success({
       ctx,
       res: {
