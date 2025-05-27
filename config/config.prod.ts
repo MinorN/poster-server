35fd47107084c09c95e5e92cfee82594849bfc90
祁嘉俊 <1481700559@qq.com>
2025-05-27 17:21:08 +0800

    feat: 初步添加docker-compose

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,6 +1,20 @@
-import { EggAppConfig, PowerPartial } from 'egg';
+import { EggAppConfig, PowerPartial } from "egg"
 
 export default () => {
-  const config = {} as PowerPartial<EggAppConfig>;
-  return config;
-};
+  const config = {} as PowerPartial<EggAppConfig>
+
+  config.mongoose = {
+    url: "mongodb://poster-mongo:27017/poster",
+  }
+
+  config.redis = {
+    client: {
+      port: 6379,
+      host: "poster-redis",
+      password: "",
+      db: 0,
+    },
+  }
+
+  return config
+}
